04c67552c48db03067c974fc8895309ea1fd2373
NItISh73 <nitish.03999@gmail.com>
2022-11-09 13:18:58 +0530

    first

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+           <div className='innerFooter'>
+                Developed by <a target='blank' href="https://github.com/NItISh73">Nitish kumar</a> | <a href="https://github.com/NItISh73/Authenticated-Quiz-App.git" target='blank'>GITHUB REPO</a>   
+            </div> 
+        </div>
+    )
+}
+export default Footer
